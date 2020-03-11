3d657af3925ef1b4d74a0f19db70fa2170189397
JhFahim <jhfahim19@gmail.com>
2020-03-11 22:46:10 +0600

    Update static files to node project structure
    - Update app.js
    - Add route for home and browse page
    - Move html files to views directory
    - Move stylesheets folder to public directory

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express"),
   app = express();
 
+// App setup
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Routes
 app.get("/", function(req, res) {
-  res.send("Welcome to homepage!");
+  res.render("home");
+});
+app.get("/browse", function(req, res) {
+  res.render("browse");
 });
 
+// Run server
 app.listen(3000, function() {
   console.log("Server has started!");
 });
