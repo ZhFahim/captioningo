207129644e19876d656d39dde7badee8f111159c
JhFahim <jhfahim19@gmail.com>
2020-03-15 19:38:28 +0600

    Add pagination system
    - Update browse route and file

--- app.js
+++ app.js
@@ -49,20 +49,36 @@ app.get("/browse", function(req, res) {
   if (req.query.category && req.query.category !== "all") {
     query.category = req.query.category;
   }
-  // Fetch captions based on query
-  Caption.find(query, function(err, captions) {
-    if (err) {
-      console.log(err);
-    } else {
-      // Render browse page based on query
-      res.render("browse", {
-        captions: captions,
-        categories: categories,
-        text: req.query.text,
-        reqCategory: req.query.category
-      });
-    }
+  // Get total page numbers based on query
+  var pages = 1;
+  Caption.countDocuments(query, function(err, count) {
+    pages = Math.ceil(count / 10); // Each page has 10 captions
   });
+  // Get current page
+  var currentPage = 1;
+  if (req.query.page) {
+    currentPage = req.query.page;
+  }
+  // Fetch captions based on query
+  Caption.find(query)
+    .limit(10)
+    .skip((currentPage - 1) * 10)
+    .sort({ $natural: -1 })
+    .exec(function(err, captions) {
+      if (err) {
+        console.log(err);
+      } else {
+        // Render browse page based on query
+        res.render("browse", {
+          captions: captions,
+          categories: categories,
+          pages: pages,
+          currentPage: currentPage,
+          queryText: req.query.text,
+          queryCategory: req.query.category
+        });
+      }
+    });
 });
 
 // Escape regex in search query
