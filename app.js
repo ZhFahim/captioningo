388392718ee21f20ad085a1ed4e049024da510f6
JhFahim <jhfahim19@gmail.com>
2020-03-16 19:26:23 +0600

    Prepare for deploying in heroku
    - Update app.js
    - Add start script

--- app.js
+++ app.js
@@ -88,6 +88,10 @@ function escapeRegex(text) {
 }
 
 // Run server
-app.listen(3000, function() {
+var port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server has started!");
 });
