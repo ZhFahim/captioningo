c82f7472b2e7c9de4129f8ac6e7ca4e849a39ff4
JhFahim <jhfahim19@gmail.com>
2020-03-17 13:33:02 +0600

    Update text copy system
    - Add clipboard.js library
    - Update script file
    - Update stylesheet file for responsiveness

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,23 @@
+// Display text copy message
+var clipboard = new ClipboardJS(".copy-btn");
 var num;
-function copyText(text) {
-  navigator.clipboard.writeText(text);
-  var msgDisplay = document.querySelector(".msg");
+var msgDisplay = document.querySelector(".msg");
+
+clipboard.on("success", function(e) {
   msgDisplay.classList.add("show");
   clearTimeout(num);
   num = setTimeout(function() {
     msgDisplay.classList.remove("show");
   }, 3000);
-}
+  console.log(e);
+});
+
+clipboard.on("error", function(e) {
+  msgDisplay.innerHTML = "Unable to copy! please copy manually.";
+  msgDisplay.classList.add("show");
+  clearTimeout(num);
+  num = setTimeout(function() {
+    msgDisplay.classList.remove("show");
+  }, 3000);
+  console.log(e);
+});
