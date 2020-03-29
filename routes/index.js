38323d483ee14129b3a5f0b88cb312a88046ba4e
JhFahim <jhfahim19@gmail.com>
2020-03-29 23:02:19 +0600

    Add show and new caption feature in dashboard
    - Show all captions in dashboard
    - Add post route to add new caption
    - Add isLoggedIn middleware

--- routes/index.js
+++ routes/index.js
@@ -107,46 +107,111 @@ router.post(
 );
 
 // Dashboard
-router.get("/dashboard", function(req, res) {
-  if (req.isAuthenticated()) {
-    // Get Statics
-    var statics = {};
-    // Total Captions
-    Caption.countDocuments({}, function(err, totalCaptions) {
-      statics.totalCaptions = totalCaptions;
-      // Total Categories
-      Caption.distinct("category", function(err, foundCategories) {
-        statics.totalCategories = foundCategories.length;
-        // Total Caption Used By
-        Caption.aggregate(
-          [
-            {
-              $group: {
-                _id: null,
-                totalUsedBy: { $sum: "$usedBy" }
-              }
+router.get("/dashboard", isLoggedIn, function(req, res) {
+  // Get Statics
+  var statics = {};
+  // Total Captions
+  Caption.countDocuments({}, function(err, totalCaptions) {
+    statics.totalCaptions = totalCaptions;
+    // Total Categories
+    Caption.distinct("category", function(err, foundCategories) {
+      statics.totalCategories = foundCategories.length;
+      // Total Caption Used By
+      Caption.aggregate(
+        [
+          {
+            $group: {
+              _id: null,
+              totalUsedBy: { $sum: "$usedBy" }
             }
-          ],
-          function(err, usedByResults) {
-            statics.totalUsedBy = usedByResults[0].totalUsedBy;
-            // Total Hits
-            Info.findOne({ name: "Hits" }, function(err, foundInfo) {
-              statics.totalHits = foundInfo.value;
-              console.log(statics);
-              res.render("dashboard", { statics: statics });
-            });
           }
-        );
-      });
+        ],
+        function(err, usedByResults) {
+          statics.totalUsedBy = usedByResults[0].totalUsedBy;
+          // Total Hits
+          Info.findOne({ name: "Hits" }, function(err, foundInfo) {
+            statics.totalHits = foundInfo.value;
+            // Display Captions
+            // Retrive all categories from DB
+            var categories;
+            Caption.distinct("category", function(err, foundCategories) {
+              if (err) {
+                console.log(err);
+              } else {
+                categories = foundCategories;
+              }
+            });
+            // Make query object
+            var query = {};
+            // Check if user requests a query or not
+            if (req.query.text) {
+              query.text = new RegExp(escapeRegex(req.query.text), "gi");
+            }
+            if (req.query.category && req.query.category !== "all") {
+              query.category = req.query.category;
+            }
+            // Get total page numbers based on query
+            var pages = 1;
+            Caption.countDocuments(query, function(err, count) {
+              pages = Math.ceil(count / 10); // Each page has 10 captions
+            });
+            // Get current page
+            var currentPage = 1;
+            if (req.query.page) {
+              currentPage = req.query.page;
+            }
+            // Fetch captions based on query
+            Caption.find(query)
+              .limit(10)
+              .skip((currentPage - 1) * 10)
+              .sort({ $natural: -1 })
+              .exec(function(err, captions) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  // Serve captions based on query
+                  res.render("dashboard", {
+                    statics: statics,
+                    captions: captions,
+                    categories: categories,
+                    pages: pages,
+                    currentPage: currentPage,
+                    queryText: req.query.text,
+                    queryCategory: req.query.category,
+                    page: "dashboard"
+                  });
+                }
+              });
+          });
+        }
+      );
     });
-  } else {
-    return res.redirect("/login");
-  }
+  });
+});
+
+// Add Caption from Dashboard
+router.post("/dashboard", isLoggedIn, function(req, res) {
+  Caption.create(req.body, function(err, caption) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Redirect to dashboard
+      res.redirect("/dashboard");
+    }
+  });
 });
 
 // Escape regex in search query
 function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
 module.exports = router;
